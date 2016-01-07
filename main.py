28750d0c2763c329840fff5ca07876110f4bb5e8
Joshua Luke Boddy <Josh@Beverleys-MacBook-Pro.local>
2016-01-07 22:02:27 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import sys, os
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+class main:
+    with mic as source:
+        print("Speak your command")
+        audioCommand = r.listen(source)
+
+    try:
+        print(r.recognize_google(audio))
+    except:
+        print("Error occured in processing audio, please try again!")
+        sys.exit()
